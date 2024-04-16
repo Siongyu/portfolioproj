d1b0af58dd91c02cc6058a0af1ad937316102055
SiongYu <loosiongyu@hotmail.com>
2024-04-16 12:03:21 +0800

    navbar for screen sizes done

---
+++ src/app/components/navBar.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { motion } from "framer-motion";
+
+const NavBar = () => {
+  return (
+  	<nav className="text-4xl font-bold phone:text-xl tablet:text-2xl">
+			<div className="flex justify-between text-eerie_black dark:text-ghost_white">
+				<motion.div
+					whileHover={{ 
+						scale: 1.1,
+						rotate: 180,
+						transition: { duration: 0.5 }}
+					}
+					whileTap={{ scale: 0.9 }}
+				>
+					<a href="/">
+						{"{ "}
+						<span className="text-outer_space dark:text-ghost_white">SY</span>
+						{" }"}
+					</a>
+				</motion.div>
+				<ul className="flex gap-10 phone:hidden">
+					<motion.li
+						whileHover={{ scale: 1.1 }}
+						whileTap={{ scale: 0.9 }}
+					>
+						<a href="#">About</a>
+					</motion.li>
+					<motion.li
+						whileHover={{ scale: 1.1 }}
+						whileTap={{ scale: 0.9 }}
+					>
+						<a href="#">Project(s)</a>
+					</motion.li>
+					<motion.li
+						whileHover={{ scale: 1.1 }}
+						whileTap={{ scale: 0.9 }}
+					>
+						<a href="#">Contact</a>
+					</motion.li>
+				</ul>
+			</div>
+    </nav>
+  );
+}
+
+export default NavBar; 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,74 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'phone': {'min': '400px', 'max': '480px'},
+      'tablet': {'min': '640px', 'max': '1024px'},
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+    colors: { 
+      'ghost_white': { 
+        DEFAULT: '#e8e9f3', 
+        100: '#21233e', 
+        200: '#41467c', 
+        300: '#6b71b1', 
+        400: '#a9add2', 
+        500: '#e8e9f3', 
+        600: '#ecedf5', 
+        700: '#f1f2f8', 
+        800: '#f6f6fa', 
+        900: '#fafbfd' 
+      }, 
+      'silver': { 
+        DEFAULT: '#cecece', 
+        100: '#292929', 
+        200: '#535353', 
+        300: '#7c7c7c', 
+        400: '#a5a5a5', 
+        500: '#cecece', 
+        600: '#d8d8d8', 
+        700: '#e2e2e2', 
+        800: '#ececec', 
+        900: '#f5f5f5' 
+      }, 
+      'french_gray': { 
+        DEFAULT: '#a6a6a8', 
+        100: '#212121', 
+        200: '#424243', 
+        300: '#626264', 
+        400: '#838386', 
+        500: '#a6a6a8', 
+        600: '#b7b7b8', 
+        700: '#c9c9ca', 
+        800: '#dbdbdc', 
+        900: '#ededed' 
+      }, 
+      'outer_space': {
+        DEFAULT: '#495159', 
+        100: '#0f1012', 
+        200: '#1d2124', 
+        300: '#2c3136', 
+        400: '#3b4148', 
+        500: '#495159', 
+        600: '#697480', 
+        700: '#8d97a1', 
+        800: '#b3bac1', 
+        900: '#d9dce0'
+      }, 
+      'eerie_black': { 
+        DEFAULT: '#1c2321', 
+        100: '#050706', 
+        200: '#0b0e0d', 
+        300: '#101413', 
+        400: '#161b1a', 
+        500: '#1c2321', 
+        600: '#43544f', 
+        700: '#6b867e', 
+        800: '#9bafaa', 
+        900: '#cdd7d4' 
+      } 
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
